2f12c4d4e12e2342af88e70287d76c7f6344b35b
cole <f8cole@gmail.com>
2025-05-07 19:45:51 -0400

    initial commit

---
+++ Raggle/Search/Core/IKnowledgeItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raggle.Search.Core;
+public interface IKnowledgeItem
+{
+    public string Format();
+    //add citation support
+}
